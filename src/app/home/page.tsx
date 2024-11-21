91edb1dd93a79dc5233551f272a63839f9e2b2de
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-11-22 01:49:03 +0530

    completed currency card

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,12 @@
-
+import CurrencyCard from "@/components/currency card/CurrencyCard"
 function page() {
   return (
-    <div>page</div>
+    <div>
+        page
+        <CurrencyCard name="test" priceChange24hr={45} symbol="xx" currentPrice={45} />
+    </div>
   )
+  
 }
 
 export default page
---
+++ src/components/currency card/CurrencyCard.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Link from "next/link";
+import { GoHeartFill } from "react-icons/go";
+
+interface CurrencyCardProps {
+  name: string;
+  symbol: string;
+  currentPrice: number;
+  priceChange24hr: number;
+}
+
+const CurrencyCard: React.FC<CurrencyCardProps> = ({
+  name,
+  symbol,
+  currentPrice,
+  priceChange24hr,
+}) => {
+  return (
+    <Link href={`/currency/${symbol.toLowerCase()}`} passHref>
+      <div className="cursor-pointer max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex items-center justify-around">
+        <div>{symbol}</div>
+        <div>
+          <div>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {name}
+            </h5>
+          </div>
+          <div>
+            <p className="mb-3 font-semibold text-gray-700 dark:text-gray-400">
+              Current Price: ${currentPrice.toFixed(2)}
+            </p>
+            <p
+              className={`mb-3 font-semibold ${
+                priceChange24hr >= 0 ? "text-green-500" : "text-red-500"
+              } dark:${
+                priceChange24hr >= 0 ? "text-green-400" : "text-red-400"
+              }`}
+            >
+              24h Change: {priceChange24hr >= 0 ? "+" : "-"}
+              {priceChange24hr.toFixed(2)}%
+            </p>
+          </div>
+
+          <div className="w-full">
+            <button className="text-right">
+              <GoHeartFill
+                size={24}
+                className="hover:text-red-600 transition-all"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default CurrencyCard;
